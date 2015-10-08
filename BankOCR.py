88987ad46d29b8df120d9daf1ccbf447394d4ad6
mueller <markus.mueller@mailbox.org>
2015-10-08 22:06:03 +0200

    Added checksum computation Added some checks

--- BankOCR.py
+++ BankOCR.py
@@ -2,8 +2,10 @@ import functools
 
 def splitDigits(code):
     lines = [l for l in code.split('\n')]
-    # assure that all lines have the same amount of characters
+    if len(lines) < 3:
+        raise ValueError("Invalid number of lines ({0})".format(len(lines)))
     numChars = max([len(l) for l in lines])
+    # assure that all lines have the same amount of characters
     def adjustLine(l):
         return l + ' ' * max(numChars-len(l), 0);
     lines = [adjustLine(l) for l in lines]
@@ -23,17 +25,39 @@ __numbers = '''
 #create a dict that maps each digit in string representation to its number (also str to keep leading 0)
 __digitMap = dict([(d,str(i)) for i,d in enumerate(splitDigits(__numbers))])
 def convertDigit(digit):
-    return __digitMap[digit]
+    try:
+        return __digitMap[digit]
+    except KeyError:
+        return '?'
 
 def convertDigits(digits):
     for d in splitDigits(digits):
         yield convertDigit(d)
 
+def checksum(number):
+    if len(number) == 9:
+        sum = 0
+        for i, n in enumerate(number):
+            try:
+                sum += int(n) * (9-i)
+            except ValueError:
+                return False;
+        return sum % 11 == 0
+    return False;
+
 def test(input, expected):
-    actual = ''.join(convertDigits(input))
+    number = ''.join(convertDigits(input))
+    actual = number
+    if '?' in actual:
+        actual += ' ILL'
+    # elif not checksum(actual):
+    #     actual += ' ERR'
+
     if (actual != expected):
         print(input)
-    print("{0} ({1})".format(actual, expected))
+        print("Got: {0}\n Expected: {1}) ".format(actual, expected))
+    else:
+        print(actual)
 
 # for k,v in __digitMap.items():
 #     print(k)
