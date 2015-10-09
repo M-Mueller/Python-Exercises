1c2f791c7488d856459aca5fff0a7d7d6afcc743
mueller <markus.mueller@mailbox.org>
2015-10-09 21:40:34 +0200

    Added recovery use case

--- BankOCR.py
+++ BankOCR.py
@@ -1,6 +1,6 @@
 import functools
 
-def splitDigits(code):
+def normalizeCode(code):
     lines = [l for l in code.split('\n')]
     if len(lines) < 3:
         raise ValueError("Invalid number of lines ({0})".format(len(lines)))
@@ -9,6 +9,11 @@ def splitDigits(code):
     def adjustLine(l):
         return l + ' ' * max(numChars-len(l), 0);
     lines = [adjustLine(l) for l in lines]
+    return '\n'.join(lines)
+
+def splitDigits(code):
+    lines = [l for l in code.split('\n')]
+    numChars = max([len(l) for l in lines])
     numDigits = numChars//3
     digits = ['']*numDigits
     for i in range(numDigits):
@@ -23,7 +28,7 @@ __numbers = '''
 |_|  ||_  _|  | _||_|  ||_| _|
 '''[1:]#remove first newline
 #create a dict that maps each digit in string representation to its number (also str to keep leading 0)
-__digitMap = dict([(d,str(i)) for i,d in enumerate(splitDigits(__numbers))])
+__digitMap = dict([(d,str(i)) for i,d in enumerate(splitDigits(normalizeCode(__numbers)))])
 def convertDigit(digit):
     try:
         return __digitMap[digit]
@@ -45,16 +50,39 @@ def checksum(number):
         return sum % 11 == 0
     return False;
 
-def test(input, expected):
-    number = ''.join(convertDigits(input))
+def recover(code):
+    recovered = []
+    def replace(code, i, c):
+        #print(code[:i] + c + code[i+1:])
+        number = ''.join(convertDigits(code[:i] + c + code[i+1:]))
+        if checksum(number):
+            recovered.append(number)
+    for i,c in enumerate(code):
+        if c == '_' or c == '|':
+            replace(code, i, ' ')
+        elif c == ' ':
+            replace(code, i, '_')
+            replace(code, i, '|')
+
+    return recovered
+
+
+def test(code, expected):
+    code = normalizeCode(code)
+    digits = splitDigits(code)
+    number = ''.join([convertDigit(d) for d in digits])
     actual = number
-    if '?' in actual:
-        actual += ' ILL'
-    # elif not checksum(actual):
-    #     actual += ' ERR'
+    if '?' in actual or not checksum(actual):
+        recovered = recover(code)
+        if not recovered:
+            actual += ' ILL'
+        elif len(recovered) == 1:
+            actual = recovered[0]
+        else:
+            actual = number + ' AMB ' + str(recovered)
 
     if (actual != expected):
-        print(input)
+        print(code)
         print("Got: {0}\n Expected: {1}) ".format(actual, expected))
     else:
         print(actual)
