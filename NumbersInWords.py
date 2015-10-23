34d231319990919e181e7c051928bfb78d4b63d8
mueller <markus.mueller@mailbox.org>
2015-10-23 23:42:57 +0200

    Added conversion from word to number

--- NumbersInWords.py
+++ NumbersInWords.py
@@ -127,3 +127,98 @@ def number_in_english(number):
     if word.startswith('and'):
         word = word.replace('and', '')
     return word.strip()
+
+def english_to_number(english):
+    """Converts words representing a number into the number.
+    Throws a ValueError if the string does not represent a number.
+    >>> english_to_number('five')
+    5
+    >>> english_to_number('twenty eight')
+    28
+    >>> english_to_number('seven hundred and fourty five')
+    745
+    >>> english_to_number(' seVen hUndred   aNd Fourty FIVE ')
+    745
+    >>> english_to_number('one thousand three hundred and eighty')
+    1380
+    >>> english_to_number('three million two hundred four thousand')
+    3204000
+    >>> english_to_number('fifteen thousand')
+    15000
+    >>> english_to_number('one thousand and five')
+    1005
+    >>> english_to_number('one thousand hundred and eighty')
+    Traceback (most recent call last):
+    ...
+    ValueError: Input is not a valid number
+    >>> english_to_number('one thousand two million')
+    Traceback (most recent call last):
+    ...
+    ValueError: Input is not a valid number
+    >>> english_to_number('one two')
+    Traceback (most recent call last):
+    ...
+    ValueError: Input is not a valid number
+    >>> english_to_number('hundred thousand')
+    Traceback (most recent call last):
+    ...
+    ValueError: Input is not a valid number
+    >>> english_to_number('not a number')
+    Traceback (most recent call last):
+    ...
+    ValueError: Input contains invalid words
+    >>> english_to_number('not seven')
+    Traceback (most recent call last):
+    ...
+    ValueError: Input contains invalid words
+    >>> english_to_number('')
+    Traceback (most recent call last):
+    ...
+    ValueError: Input is not representing a number
+    """
+    if not english or english.isspace():
+        raise ValueError('Input is not representing a number')
+    words = [w.strip().lower() for w in english.split()]
+
+    number = 0
+    power = 1
+    allowed_next = [1, 10, 100, 1000] # holds which numbers are possible next
+    # start with the lower number and sum up the converted number
+    for word in reversed(words):
+        if word in numbers:
+            i = numbers.index(word)
+            if i < 10:
+                if 1 not in allowed_next:
+                    raise ValueError('Input is not a valid number')
+                allowed_next = [10, 100, 1000]
+            else: #number is actually a tenner
+                if 10 not in allowed_next:
+                    raise ValueError('Input is not a valid number')
+                allowed_next = [100, 1000]
+            number += numbers.index(word)*power
+        elif word in tenner:
+            if 10 not in allowed_next:
+                raise ValueError('Input is not a valid number')
+            allowed_next = [100, 1000]
+            number += tenner.index(word)*10
+        elif word == 'hundred':
+            if 100 not in allowed_next:
+                raise ValueError('Input is not a valid number')
+            allowed_next = [1]
+            power *= 100
+        elif word in powers:
+            if 1000 not in allowed_next:
+                raise ValueError('Input is not a valid number')
+            allowed_next = [1, 10, 100]
+            p = 1000**powers.index(word)
+            if p <= power:
+                raise ValueError('Input is not a valid number')
+            else:
+                power = p
+        elif word == 'and':
+            pass
+        else:
+            raise ValueError('Input contains invalid words')
+    if number == 0:
+        raise ValueError('Input is not a valid number')
+    return number
